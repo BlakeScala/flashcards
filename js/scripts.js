76ca4d706c262904b6d3dffdeff2b26e8b29cab1
Blake Scala and Seth Kendall <blake@>
2016-08-09 13:23:13 -0700

    update with ability to toggle answers.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,95 @@
 $(function() {
-  $(".opener h2").click(function() {
-    $(".opener p").toggle();
+  $("#opener1 h2").click(function() {
+    $("#opener1 p").toggle();
+  });
+});
+$(function() {
+  $("#opener2 h2").click(function() {
+    $("#opener2 p").toggle();
+  });
+});
+$(function() {
+  $("#opener3 h2").click(function() {
+    $("#opener3 p").toggle();
+  });
+});
+$(function() {
+  $("#opener4 h2").click(function() {
+    $("#opener4 p").toggle();
+  });
+});
+$(function() {
+  $("#opener5 h2").click(function() {
+    $("#opener5 p").toggle();
+  });
+});
+$(function() {
+  $("#opener6 h2").click(function() {
+    $("#opener6 p").toggle();
+  });
+});
+$(function() {
+  $("#opener7 h2").click(function() {
+    $("#opener7 p").toggle();
+  });
+});
+$(function() {
+  $("#opener8 h2").click(function() {
+    $("#opener8 p").toggle();
+  });
+});
+$(function() {
+  $("#opener9 h2").click(function() {
+    $("#opener9 p").toggle();
+  });
+});
+$(function() {
+  $("#opener10 h2").click(function() {
+    $("#opener10 p").toggle();
+  });
+});
+$(function() {
+  $("#opener11 h2").click(function() {
+    $("#opener11 p").toggle();
+  });
+});
+$(function() {
+  $("#opener12 h2").click(function() {
+    $("#opener12 p").toggle();
+  });
+});
+$(function() {
+  $("#opener13 h2").click(function() {
+    $("#opener13 p").toggle();
+  });
+});
+$(function() {
+  $("#opener14 h2").click(function() {
+    $("#opener14 p").toggle();
+  });
+});
+$(function() {
+  $("#opener15 h2").click(function() {
+    $("#opener15 p").toggle();
+  });
+});
+$(function() {
+  $("#opener16 h2").click(function() {
+    $("#opener16 p").toggle();
+  });
+});
+$(function() {
+  $("#opener17 h2").click(function() {
+    $("#opener17 p").toggle();
+  });
+});
+$(function() {
+  $("#opener18 h2").click(function() {
+    $("#opener18 p").toggle();
+  });
+});
+$(function() {
+  $("#opener19 h2").click(function() {
+    $("#opener19 p").toggle();
   });
 });
